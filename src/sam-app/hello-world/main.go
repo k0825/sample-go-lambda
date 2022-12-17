3a8568f3bc25d7c055b859edb811628f1bd2cbb4
k0825 <kazukkiyo@icloud.com>
2022-12-17 21:56:45 +0900

    SAMのファイルをsrc下に配置

--- src/sam-app/hello-world/main.go
+++ src/sam-app/hello-world/main.go
@@ -11,9 +11,14 @@ import (
 )
 
 var (
+	// DefaultHTTPGetAddress Default Address
 	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
-	ErrNoIP               = errors.New("No IP in HTTP response")
-	ErrNon200Response     = errors.New("Non 200 Response found")
+
+	// ErrNoIP No IP found in response
+	ErrNoIP = errors.New("No IP in HTTP response")
+
+	// ErrNon200Response non 200 status code in response
+	ErrNon200Response = errors.New("Non 200 Response found")
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
